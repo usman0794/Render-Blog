76f5eed4b30fb7501fe4aceeb91aa44c4e9f9b0e
Muhammad Usman Ali <usmanjami794@gmail.com>
2025-01-11 21:29:10 +0500

    Fix tailwind content configuration

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,11 +1,15 @@
-import flowbite from "flowbite-react/tailwind";
-import tailwindScrollbar from "tailwind-scrollbar";
-
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", ...flowbite.content],
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+  ],
   theme: {
     extend: {},
   },
-  plugins: [flowbite.plugin, tailwindScrollbar],
+  plugins: [
+    require('flowbite/plugin'),
+    require('tailwind-scrollbar'),
+  ],
 };
