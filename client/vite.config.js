12cb9c83e681f3a9c3e73a73e6e952ac880b9368
Muhammad Usman Ali <usmanjami794@gmail.com>
2025-01-11 22:17:55 +0500

    fix vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,23 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "http://localhost:3000",
+        target: "http://localhost:5000", // Change to port 5000
+        changeOrigin: true,
         secure: false,
       },
     },
   },
   plugins: [react()],
   optimizeDeps: {
-    include: ["@aws-sdk/client-s3"], // Pre-bundle the AWS SDK dependency
+    include: ["@aws-sdk/client-s3"],
   },
   resolve: {
-    alias: {
-      // Add custom aliases if required
-    },
+    alias: {},
   },
 });
