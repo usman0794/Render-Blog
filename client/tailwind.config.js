a705a5348a4e7f0ee17083d3ef4a3047d3dd7c1a
Muhammad Usman Ali <usmanjami794@gmail.com>
2025-01-11 18:55:35 +0500

    Fix ESM syntax in tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,13 +1,14 @@
-const flowbite = require("flowbite-react/tailwind");
-/** @type {import('tailwindcss').Config} */
+import flowbite from "flowbite-react/tailwind";
+import tailwindScrollbar from "tailwind-scrollbar";
 
+/** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", ...flowbite.content],
   theme: {
     extend: {},
   },
   plugins: [
-    flowbite.plugin(),
-    require("tailwind-scrollbar"),
+    flowbite.plugin,
+    tailwindScrollbar,
   ],
 };
